3f6bcb81c7eb43ab00e7ae9f71dcf4a79d4320e2
root <root@localhost.localdomain>
2021-10-19 17:06:08 +0000

    Recent winner in comp page

--- src/pages/competition-page/competition-page.jsx
+++ src/pages/competition-page/competition-page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Competition from "../../sections/competition/competition";
+import RecentWinners from "../../sections/recent-winners/recent-winners";
 
 export default function CompetitionPage(props) {
     const competitionId = props.location.pathname.replace('/competition/', '');
@@ -8,6 +9,7 @@ export default function CompetitionPage(props) {
     return (
         <QueryClientProvider client={new QueryClient()}>
             <Competition id={competitionId}></Competition>
+            <RecentWinners />
         </QueryClientProvider>
     )
 }
